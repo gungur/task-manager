ae5e4affe6f6c3b09a5dd354464dd75ebf14f7e3
Sai Gungurthi <98351945+gungur@users.noreply.github.com>
2026-04-13 22:48:06 +0530

    Day 1: Initial Spring Boot setup and hello endpoint

---
+++ src/main/java/com/sai/notionlite/NotionliteApplication.java
@@ -0,0 +1,13 @@
+package com.sai.notionlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/notionlite/NotionliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.notionlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
